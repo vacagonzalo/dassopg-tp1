c0d09090e926716e51eba786d57c391feb45256f
Gonzalo Nahuel Vaca <vacagonzalo@gmail.com>
2022-03-20 12:01:32 -0300

    cvs to json

---
+++ parser_service.py
@@ -0,0 +1,62 @@
+#!bin/python
+import signal
+import os
+from re import search
+from time import sleep
+from json import dumps as to_json
+
+
+class Parser():
+    def __init__(self, source_file: str):
+        self.pattern = "[0-9]+,[a-zA-Z]+,[0-9]+.[0-9]+,[0-9]+.[0-9]+"
+        self.file = source_file
+
+    def __del__(self):
+        pass
+
+    def valid_syntax(self, line: str) -> bool:
+        return search(self.pattern, line)
+
+    def parse(self) -> str:
+        msg = []
+        with open(self.file, 'r') as file:
+            for line in file:
+                clean_line = line.strip()
+                if self.valid_syntax(clean_line):
+                    data = clean_line.split(',')
+                    coin = {'id': data[0],
+                            'value1': data[1],
+                            'value2': data[2],
+                            'name': data[3]}
+                    msg.append(coin)
+        return to_json(msg)
+
+
+def signal_handler(signum, frame):
+    print('Signal handler called with signal', signum)
+    raise OSError("Program ended by external signal")
+
+
+def main():
+    print("Ingrese el camino y nombre del archivo")
+    file_name = input("> ")
+
+    print("Ingrese el host del servicio pizarra")
+    host = input("> ")
+
+    print("Ingrese el puerto del servicio pizarra")
+    port = input("> ")
+
+    parser = Parser(file_name)
+
+    while True:
+        print(parser.parse())
+        sleep(1)
+
+
+if __name__ == "__main__":
+    signal.signal(signal.SIGINT, signal_handler)
+    try:
+        main()
+    except OSError as e:
+        print(e)
